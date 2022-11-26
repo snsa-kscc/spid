d36f1fcdab62b07e52adfd4f32db1526b0bc9226
Sinisa Koscec <sinisa@dvasadva.com>
2022-11-27 00:28:07 +0100

    initial

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+import "../styles/tailwind.css";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+import BlogPost from "../components/BlogPost";
+import Feature from "../components/Feature";
+import FeaturedPosts from "../components/FeaturedPosts";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import PostsList from "../components/PostsList";
+import Teaser from "../components/Teaser";
+import Text from "../components/Text";
+
+const components = {
+  feature: Feature,
+  "featured-posts": FeaturedPosts,
+  grid: Grid,
+  page: Page,
+  post: BlogPost,
+  "selected-posts": PostsList,
+  teaser: Teaser,
+  text: Text,
+};
+
+storyblokInit({
+  accessToken: "9LsVdWY9CrrBfqvJqmqAjwtt",
+  use: [apiPlugin],
+  components,
+});
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
