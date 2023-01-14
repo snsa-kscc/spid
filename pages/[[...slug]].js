0194e78c0fd204f6df11c8305309c62f581cf535
Sinisa Koscec <sinisa@dvasadva.com>
2023-01-14 02:22:54 +0100

    draft to published

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -18,7 +18,7 @@ export async function getStaticProps({ locale, locales, defaultLocale, params })
   let slug = params.slug ? params.slug.join("/") : "home";
 
   let sbParams = {
-    version: "draft",
+    version: "published",
     resolve_relations: ["featured-posts.posts", "selected-posts.posts"],
     language: locale,
   };
