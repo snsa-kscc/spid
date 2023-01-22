517d6d47bb914dad82379b71b6b86cf54e1bc76e
Sinisa Koscec <sinisa@dvasadva.com>
2023-01-22 22:01:42 +0100

    date time

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -6,17 +6,14 @@ const BlogPost = ({ blok }) => {
     <div {...storyblokEditable(blok)}>
       <div className="bg-white-half w-full">
         <div className="max-w-3xl mx-auto text-center pt-20 flex flex-col items-center">
-          <h1 className="text-5xl font-bold font-serif text-primary tracking-wide">
-            {blok.title}
-          </h1>
+          <h1 className="text-5xl font-bold font-serif text-primary tracking-wide">{blok.title}</h1>
           <p className="text-gray-500 text-lg max-w-lg">{blok.intro}</p>
+          <p className="text-gray-500 text-lg max-w-lg">{blok.date}</p>
           <img className="w-full bg-gray-300 my-16" src={blok.image} />
         </div>
       </div>
       <div className="max-w-3xl mx-auto text-center pt-20 flex flex-col items-center">
-        <div className="leading-relaxed text-xl text-left text-gray-800 drop-cap">
-          {render(blok.long_text)}
-        </div>
+        <div className="leading-relaxed text-xl text-left text-gray-800 drop-cap">{render(blok.long_text)}</div>
       </div>
     </div>
   );
