b727af24578f33f4ea9e81161201ef5cd96cbf38
Sinisa Koscec <sinisa@dvasadva.com>
2023-01-22 23:14:14 +0100

    date time format

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -2,13 +2,16 @@ import { storyblokEditable } from "@storyblok/react";
 import { render } from "storyblok-rich-text-react-renderer";
 
 const BlogPost = ({ blok }) => {
+  const date = new Date(blok.date);
+  const dateFormat = new Intl.DateTimeFormat("hr");
+
   return (
     <div {...storyblokEditable(blok)}>
       <div className="bg-white-half w-full">
         <div className="max-w-3xl mx-auto text-center pt-20 flex flex-col items-center">
           <h1 className="text-5xl font-bold font-serif text-primary tracking-wide">{blok.title}</h1>
           <p className="text-gray-500 text-lg max-w-lg">{blok.intro}</p>
-          <p className="text-gray-500 text-lg max-w-lg">{blok.date}</p>
+          <p className="text-gray-500 text-lg max-w-lg">{dateFormat.format(date)}</p>
           <img className="w-full bg-gray-300 my-16" src={blok.image} />
         </div>
       </div>
