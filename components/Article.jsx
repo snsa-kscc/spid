32973f10a55a929de27edbf4c7b85380df13a2fc
Sinisa Koscec <sinisa@dvasadva.com>
2023-01-30 04:08:26 +0100

    youtube renderer

--- components/Article.jsx
+++ components/Article.jsx
@@ -1,9 +1,35 @@
 import { storyblokEditable } from "@storyblok/react";
-import { render } from "storyblok-rich-text-react-renderer";
+import { render, MARK_LINK } from "storyblok-rich-text-react-renderer";
 
 const Article = ({ blok, story }) => {
   const date = new Date(story.first_published_at);
   const dateFormat = new Intl.DateTimeFormat("hr");
+
+  const options = {
+    markResolvers: {
+      [MARK_LINK]: node => {
+        if (node.includes("youtube.com")) {
+          const match = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/.exec(node)
+          const videoId =
+            match && match[7].length === 11 ? match[7] : null
+          return (
+            videoId && (
+              <section className="video-container">
+                <iframe
+                  className="video"
+                  title={`https://youtube.com/embed/${videoId}`}
+                  src={`https://youtube.com/embed/${videoId}`}
+                  allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                  frameBorder="0"
+                  allowFullScreen
+                />
+              </section>
+            )
+          )
+        }
+      },
+    }
+  }
 
   return (
     <div {...storyblokEditable(blok)}>
@@ -15,7 +41,7 @@ const Article = ({ blok, story }) => {
         </div>
       </div>
       <div className="max-w-3xl mx-auto text-center pt-20 flex flex-col items-center">
-        <div className="leading-relaxed text-xl text-left text-gray-800 drop-cap">{render(blok.long_text)}</div>
+        <div className="leading-relaxed text-xl text-left text-gray-800 drop-cap">{render(blok.long_text, options)}</div>
       </div>
     </div>
   );
