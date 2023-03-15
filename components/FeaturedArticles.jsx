db532db5f54e2c73c44c4420dd8afea8339a67f4
Sinisa Koscec <sinisa@dvasadva.com>
2023-03-15 23:00:05 +0100

    featured news styles fix

--- components/FeaturedArticles.jsx
+++ components/FeaturedArticles.jsx
@@ -4,7 +4,7 @@ import AllArticles from "./AllArticles";
 const FeaturedArticles = ({ blok, locale }) => {
   if (locale === 'hr') {
     return (
-      <div {...storyblokEditable(blok)} className="py-20 md:py-40 mb-6 container mx-auto text-left">
+      <div {...storyblokEditable(blok)} className="featured py-20 md:py-40 mb-6 container mx-auto text-left">
         <div className="container grid place-items-center my-20">
           <div className="inline-block bg-[#B1D2F5] px-14 py-6 lg:px-24 sm:py-8 border-4 border-black rounded-lg shadow-spid mb-12">
             <p className="font-mono text-xl sm:text-3xl">{blok.title}</p>
