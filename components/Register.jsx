447b1811b9f38bc91cf1e9fa3128c0af4ab6ce55
Sinisa Koscec <sinisa@dvasadva.com>
2023-03-19 14:11:38 +0100

    steps counter

--- components/Register.jsx
+++ components/Register.jsx
@@ -3,9 +3,14 @@ import { render } from "storyblok-rich-text-react-renderer";
 import Link from "next/link";
 
 const Register = ({ blok }) => {
+  const stepsArray = blok.steps.split(",").map(step => step.trim())
   return (
     <div {...storyblokEditable(blok)} className="my-40 container mx-auto">
-      <p>{blok.steps}</p>
+      <div className="steps">
+        {stepsArray.map((item, idx) => {
+          return <div key={idx} className="step">{item}</div>
+        })}
+      </div>
       <div>{render(blok.paymentInfo)}</div>
       <Link href={blok.criteria.cached_url}>Kriteriji za članstvo</Link>
       <a href={blok.form.url} target={blok.form.target}>{blok.form.title}</a>
