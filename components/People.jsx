7796ce79dc0526fd2d2bea62c068e8b9e3840e05
Sinisa Koscec <sinisa@dvasadva.com>
2023-03-21 11:09:45 +0100

    people logic

--- components/People.jsx
+++ components/People.jsx
@@ -14,25 +14,65 @@ const Column = ({ entries }) => {
 
 const People = ({ blok }) => {
 
-  const names = blok.regularMembers
+  const entriesPerColumn = 11;
+
+  const namesR = blok.regularMembers
     .split(";")
     .map((person) => person.split(","))
     .sort((a, b) => a[1].localeCompare(b[1], "hr"))
     .map((person) => `${person[0].trim()}, ${person[1].trim()}`);
 
-  const entriesPerColumn = 11;
-  let columns = [];
-  for (let i = 0; i < names.length; i += entriesPerColumn) {
-    columns.push(names.slice(i, i + entriesPerColumn));
+  let columnsR = [];
+  for (let i = 0; i < namesR.length; i += entriesPerColumn) {
+    columnsR.push(namesR.slice(i, i + entriesPerColumn));
   }
 
-  const lastEntries = columns
-    .slice(0, -1)
+  const totalPeopleR = namesR.length
+
+  const lastEntriesR = columnsR
     .map((column) => column.at(-1));
 
-  const firstLetterOfLastEntries = lastEntries.map(entry => {
+  const firstEntriesR = columnsR
+    .map((column) => column.at(0));
+
+  const firstLetterOfLastEntriesR = lastEntriesR.map(entry => {
     return entry.split(", ")[1][0].toUpperCase()
   })
+
+  const firstLetterOfFirstEntriesR = firstEntriesR.map(entry => {
+    return entry.split(", ")[1][0].toUpperCase()
+  })
+
+  const letterPairsR = firstLetterOfFirstEntriesR.map((element, index) => [element, firstLetterOfLastEntriesR[index]]);
+
+  const namesL = blok.associatedMembers
+    .split(";")
+    .map((person) => person.split(","))
+    .sort((a, b) => a[1].localeCompare(b[1], "hr"))
+    .map((person) => `${person[0].trim()}, ${person[1].trim()}`);
+
+  let columnsL = [];
+  for (let i = 0; i < namesL.length; i += entriesPerColumn) {
+    columnsL.push(namesL.slice(i, i + entriesPerColumn));
+  }
+
+  const totalPeopleL = namesL.length
+
+  const lastEntriesL = columnsL
+    .map((column) => column.at(-1));
+
+  const firstEntriesL = columnsL
+    .map((column) => column.at(0));
+
+  const firstLetterOfLastEntriesL = lastEntriesL.map(entry => {
+    return entry.split(", ")[1][0].toUpperCase()
+  })
+
+  const firstLetterOfFirstEntriesL = firstEntriesL.map(entry => {
+    return entry.split(", ")[1][0].toUpperCase()
+  })
+
+  const letterPairsL = firstLetterOfFirstEntriesL.map((element, index) => [element, firstLetterOfLastEntriesL[index]]);
 
   // const letterPairs = firstLetterOfLastEntries.reduce((acc, letter, index, array) => {
   //   const isFirstLetter = index === 0;
@@ -52,56 +92,71 @@ const People = ({ blok }) => {
   //   return acc;
   // }, []).filter(currentPair => currentPair[0] !== currentPair[1] && currentPair[0] !== 'Ž');
 
-  const letterPairs = firstLetterOfLastEntries.reduce((acc, letter, index, array) => {
-    const isFirstLetter = index === 0;
-    const isLastLetter = index === array.length - 1;
+  // const letterPairs = firstLetterOfLastEntries.reduce((acc, letter, index, array) => {
+  //   const isFirstLetter = index === 0;
+  //   const isLastLetter = index === array.length - 1;
 
-    if (isFirstLetter) {
-      acc.push(['A', letter]);
-    } else if (!isLastLetter) {
-      acc.push([array[index - 1], letter]);
-    } else {
-      const lastPair = acc[acc.length - 1];
-      if (lastPair) {
-        acc.push([lastPair[1], letter]);
-      }
-      acc.push([letter, 'Ž']);
-    }
+  //   if (isFirstLetter) {
+  //     acc.push(['A', letter]);
+  //   } else if (!isLastLetter) {
+  //     acc.push([array[index - 1], letter]);
+  //   } else {
+  //     const lastPair = acc[acc.length - 1];
+  //     if (lastPair) {
+  //       acc.push([lastPair[1], letter]);
+  //     }
+  //     acc.push([letter, 'Ž']);
+  //   }
 
-    return acc;
-  }, []);
-
-  const firstEntryLastColumn = columns.at(-1)[0];
+  //   return acc;
+  // }, []);
 
   return (
     <div {...storyblokEditable(blok)}>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          alignItems: "flex-start",
-          justifyContent: "space-between"
-        }}
-      >
-        {columns.map((column, index) => (
-          <Column entries={column} key={index} />
-        ))}
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "flex-start",
+            justifyContent: "space-between"
+          }}
+        >
+          {columnsR.map((column, index) => (
+            <Column entries={column} key={index} />
+          ))}
+        </div>
+        <p>Ranges</p>
+        <ul>
+          {letterPairsR.map((pair, index) => (
+            <li key={index}>od {pair[0]} do {pair[1]}</li>
+          ))}
+        </ul>
+        <p>total {totalPeopleR}</p>
       </div>
-      <h3>Last Entry from every Column except last one</h3>
-      <ul>
-        {lastEntries.map((entry) => (
-          <li key={entry}>{entry.split(", ")[1][0].toUpperCase()}</li>
-        ))}
-      </ul>
-      <h3>First Entry from the Last Column</h3>
-      <p>{firstEntryLastColumn.split(", ")[1][0].toUpperCase()}</p>
-      <p>ranges</p>
-      <ul>
-        {letterPairs.map((pair, index) => (
-          <li key={index}>od {pair[0]} do {pair[1]}</li>
-        ))}
-      </ul>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "flex-start",
+            justifyContent: "space-between"
+          }}
+        >
+          {columnsL.map((column, index) => (
+            <Column entries={column} key={index} />
+          ))}
+        </div>
+        <p>Ranges</p>
+        <ul>
+          {letterPairsL.map((pair, index) => (
+            <li key={index}>od {pair[0]} do {pair[1]}</li>
+          ))}
+        </ul>
+        <p>total {totalPeopleL}</p>
+      </div>
     </div>
+
   );
 }
 
